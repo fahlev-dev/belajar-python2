1eed44b63f8c37bae222e6148a8ce56740cfc9d2
Reza Fahlevi <rfahlevi501@gmail.com>
2022-12-24 16:49:30 +0700

    menambahkan file game dan draw

---
+++ mygame/game.py
@@ -0,0 +1,17 @@
+# game.py
+# import the draw module
+from draw import draw_game
+
+
+def play_game():
+    ...
+
+def main():
+    result = play_game()
+    draw.draw_game(result)
+
+# this means that if this script is executed, then 
+# main() will be executed
+
+if __name__ == '__main__':
+    main()
