ed53b00810a2eb8e26e3b0cb50f80dde21ea1c09
Reza Fahlevi <rfahlevi501@gmail.com>
2023-01-01 08:45:53 +0700

    learn with mosh

---
+++ learn_with_mosh.py
@@ -0,0 +1,47 @@
+# name = input("What is your name: ")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year: ")
+# age = 2022 - int(birth_year)
+# print(age)
+
+# first_number = int(input("Enter first number : "))
+# second_number = int(input("Enter second number : "))
+# resultSum = first_number + second_number
+# print(f"result sum is {resultSum}")
+
+# temperature = 25
+
+# if temperature > 30:
+#     print("it's is a hot day")
+# elif temperature > 20 or temperature < 30:
+#     print("it's a good day")
+
+# berat = int(input("Masukan berat badan anda: "))
+# unit = input("(K)g atau (P)nds: ")
+# if unit.upper() == "K":
+#     konversi = berat / 0.45
+#     print("Berat badan anda dalam ponds : " + str(konversi))
+# else:
+#     konversi = berat * 0.45
+#     print("Berat badan anda kg : " + str(konversi))
+# print("Program Selesai")
+
+# names = ["John", "Bob", "Mosh", "Sam", "Mary"]
+# names[0] = "Jon"
+# print(names[0:3])
+
+# numbers = [1,2,3,4,5]
+# numbers.insert(6)
+# print(numbers)
+
+# numbers = [1,2,3,4,5]
+# for item in numbers:
+#     print(item)
+
+# i = 0
+# while i < len(numbers):
+#     print(numbers[i])
+#     i = i + 1
+
+
