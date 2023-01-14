61028688f5a37f6517be5003c541167a7da43146
Reza Fahlevi <rfahlevi501@gmail.com>
2023-01-14 21:18:59 +0700

    new edit

---
+++ learn_exception.py
@@ -0,0 +1,8 @@
+a = ["Python", "Exception", "Try and except"]
+
+try:
+    for i in range(4):
+        print("The index and element from the array is", i,a[i])
+except:
+    print("index out of range")
+    
